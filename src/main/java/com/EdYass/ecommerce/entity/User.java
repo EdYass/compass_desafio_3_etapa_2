5c5049465f25163417cebfdc0ccf7e040d33a465
Edwin Yassunaga <edwinjunyassunaga@gmail.com>
2024-07-12 17:16:03 -0300

    Add User entity with email, password, roles, and resetToken fields

---
+++ src/main/java/com/EdYass/ecommerce/entity/User.java
@@ -0,0 +1,36 @@
+// src/main/java/com/EdYass/ecommerce/entity/User.java
+package com.EdYass.ecommerce.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+
+    private String resetToken;
+}
